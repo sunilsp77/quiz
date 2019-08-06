621e6d291757ff2df5b2644ea77538601598b030
Sunil Prakash <sunil.thind17@gmail.com>
2019-08-07 01:17:36 +0530

    Cleaning code2

--- src/components/WelcomeScreen/WelcomeScreen.js
+++ src/components/WelcomeScreen/WelcomeScreen.js
@@ -4,11 +4,8 @@ import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
   card: {
@@ -38,15 +35,6 @@ const useStyles = makeStyles({
   linkDiv: {
     textAlign: 'center',
   },
-
-  // link: {
-  //   fontSize: 30,
-  //   textAlign: 'center',
-  //   textDecoration: 'none',
-  //   border: '1px solid red',
-  //   backgroundColor: 'grey',
-  //   width: 'auto',
-  // },
 });
 
 export default function MediaCard() {
