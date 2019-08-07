8e56ddf38602b45ff150c3f4a64fbe25237dc0f9
Sunil Prakash <sunil.thind17@gmail.com>
2019-08-07 08:57:05 +0530

    Added comments

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+// instance of axios library to send requests to server, with base url set
 const instance = axios.create({
   baseURL: 'https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean',
 });
